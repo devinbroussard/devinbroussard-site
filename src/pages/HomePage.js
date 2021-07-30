77f78928c7400d9711ba4ac133cedf9f6a80f1dd
Devin Broussard <devin.ray.broussard@gmail.com>
2021-07-30 16:28:17 -0500

    update

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import React, { Component } from 'react';
+
+class HomePage extends React.Component {
+    render() {
+      return (
+        <div>
+            <Navbar />
+            <body class="background">
+
+            </body>
+        </div>
+      )
+    }
+  }
