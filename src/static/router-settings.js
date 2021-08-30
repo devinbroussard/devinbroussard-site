b65b9e6c43f34e63f64dd31b1585c0cd6a77126d
Devin Broussard <devin.ray.broussard@gmail.com>
2021-08-30 13:32:19 -0500

    Added dynamic loading to router

--- src/static/router-settings.js
+++ src/static/router-settings.js
@@ -1,47 +1,36 @@
 import React from "react";
-import AboutPage from "../pages/AboutPage";
-import HomePage from "../pages/HomePage";
 
-const pages = [
-  "HomePage",
-  "AboutPage",
-  "ExperiencePage",
-  "ProjectsPage",
-  "ContactPage",
-];
-
-const page = Array.map(pages);
-
+//Array used to store objects containing information on each page
 const data = [
   {
     name: "home",
     redirect: "/",
     className: "active",
-    linkTo: React.lazy(() => import("../pages/HomePage")),
+    linkTo: React.lazy(() => import(`../pages/HomePage`)),
   },
   {
     name: "about",
     redirect: "/about",
     className: "",
-    linkTo: React.lazy(() => import("../pages/AboutPage")),
+    linkTo: React.lazy(() => import(`../pages/AboutPage`)),
   },
   {
     name: "experience",
     redirect: "/experience",
     className: "",
-    linkTo: "",
+    linkTo: React.lazy(() => import(`../pages/ExperiencePage`)),
   },
   {
     name: "projects",
     redirect: "/project",
     className: "",
-    linkTo: "",
+    linkTo: React.lazy(() => import(`../pages/ProjectsPage`)),
   },
   {
     name: "contact",
     redirect: "/contact",
     className: "",
-    linkTo: "",
+    linkTo: React.lazy(() => import(`../pages/ContactPage`)),
   },
 ];
 
