03431b3dbe31ba05d2ed8fdcc428bc169e9c8174
Devin Broussard <devin.ray.broussard@gmail.com>
2021-09-05 18:19:15 -0500

    Updated Home Page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,12 +6,14 @@ import "../styles/HomePage.css";
 function HomePage() {
   
   return (
-    <div className="title-container">
-      <h1 className="pretitle highlight">HELLO, MY NAME IS DEVIN</h1>
-      <span className="title">I make software.</span>
-      <h1 className="endtitle highlight d-flex justify-content-end">
-        HERE IS MY RESUME
-      </h1>
+    <div className="">
+      <div className="title-container px-auto">
+        <h1 className="pretitle highlight">HELLO, MY NAME IS DEVIN</h1>
+        <span className="title">I make software.</span>
+        <h1 className="endtitle highlight d-flex justify-content-end">
+          HERE IS MY RESUME
+        </h1>
+      </div>
     </div>
   );
 }
