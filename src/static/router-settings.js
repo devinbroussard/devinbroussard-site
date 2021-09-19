a34c6284816e0b8537c1d010b45f63bda0849fe8
Devin Broussard <devin.ray.broussard@gmail.com>
2021-09-19 15:34:30 -0500

    Working active class (Non-responsive)

--- src/static/router-settings.js
+++ src/static/router-settings.js
@@ -6,31 +6,26 @@ const data = [
     name: "home",
     redirect: "/",
     component: React.lazy(() => import(`../pages/HomePage`)),
-    isActive: false
   },
   {
     name: "about",
     redirect: "/about",
     component: React.lazy(() => import(`../pages/AboutPage`)),
-    isActive: false
   },
   {
     name: "experience",
     redirect: "/experience",
     component: React.lazy(() => import(`../pages/ExperiencePage`)),
-    isActive: false
   },
   {
     name: "projects",
     redirect: "/project",
     component: React.lazy(() => import(`../pages/ProjectsPage`)),
-    isActive: false
   },
   {
     name: "contact",
     redirect: "/contact",
     component: React.lazy(() => import(`../pages/ContactPage`)),
-    isActive: false
   },
 ];
 
