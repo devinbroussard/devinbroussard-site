0827e03e0f6f5ad97f1d852c77c3fceebd31ab45
Devin Broussard <devin.ray.broussard@gmail.com>
2021-09-21 16:39:03 -0500

    Experimenting with Bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useState, Suspense, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Switch, Route, Link, NavLink } from "react-router-dom";
 import "../styles/Navbar.css";
 import routersettings from "../static/router-settings";
 import face from "../data/face.png";
@@ -9,14 +9,14 @@ function Navbar() {
   return (
     <div>
       <div className="header-background"/>
-      <div className="navbar-expand navbar-custom row navbar-nav container-sm position-absolute px-3">
-        <div className="col-6">
+      <nav className="navbar navbar-expand-lg navbar-custom navbar-nav container-sm">
+        <div className="navbar-brand">
           <Link to="/" className="navbar-font nav-nav fw-bold mt-4">
-            <img src={face} alt="Face" width="75" height="75" className="me-2"/>
+            <img src={face} alt="Face" width="75" height="75" className="me-2 "/>
             Devin Broussard
           </Link>
         </div>
-        <div className="navbar-nav col-6 justify-content-end">
+        <div className="navbar-nav justify-content-end">
           {routersettings.map((routerItem, index) => (
             <NavLink to={routerItem.redirect} key={index} className="nav-link navbar-font hover-color" activeClassName="active" exact>
                 <span className="fw-bold">0{index + 1} </span>
@@ -24,7 +24,7 @@ function Navbar() {
             </NavLink>
           ))}
         </div>
-      </div>
+      </nav>
       <Switch>
         <Suspense fallback={<div>Loading...</div>}>
           {routersettings.map((routerItem, index) => (
