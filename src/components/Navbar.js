ad210d11dd6597665140d4b1c1bb0f0a9f7c60eb
Devin Broussard <devin.ray.broussard@gmail.com>
2021-09-23 19:27:28 -0500

    Finished navbar (responsive)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
   return (
     <div>
       <div className="header-background"/>
-      <nav className="navbar navbar-expand-xl navbar-custom navbar-nav container-sm">
+      <nav className="navbar navbar-light navbar-expand-lg navbar-custom navbar-nav container-sm">
         <div className="navbar-brand">
           <Link to="/" className="navbar-font nav-nav fw-bold mt-4">
             <img src={face} alt="Face" width="75" height="75" className="me-2 "/>
@@ -20,11 +20,11 @@ function Navbar() {
          data-toggle="collapse" data-bs-target="#navbarNav" 
          aria-controls="#navbarNav" aria-expanded="false"
         aria-label="Toggle navigation">
-          Menu
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="navbar-nav justify-content-end collapse navbar-collapse" id="navbarNav">
           {routersettings.map((routerItem, index) => (
-            <NavLink to={routerItem.redirect} key={index} className="nav-link navbar-font hover-color" activeClassName="active" exact>
+            <NavLink to={routerItem.redirect} key={index} className="navbar-font hover-color mx-2" activeClassName="active" exact>
                 <span className="fw-bold">0{index + 1} </span>
                 {routerItem.name}
             </NavLink>
