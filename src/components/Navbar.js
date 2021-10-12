dcac7ecd7f4f0e215c4b579d52986c36d7f4742a
Devin Broussard <devin.ray.broussard@gmail.com>
2021-10-12 16:39:14 -0500

    Experimented with the design of the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,21 +9,21 @@ function Navbar() {
   return (
     <div>
       <div className="header-background"/>
-      <nav className="navbar navbar-light navbar-expand-lg navbar-custom navbar-nav container-sm">
+      <nav className="navbar navbar-light navbar-expand-xxl navbar-custom navbar-nav container-sm">
         <div className="navbar-brand">
-          <Link to="/" className="navbar-font nav-nav fw-bold mt-4">
+          <Link to="/" className="navbar-font nav-nav fw-bold mt-4 fs-2">
             <img src={face} alt="Face" width="75" height="75" className="me-2 "/>
             Devin Broussard
           </Link>
         </div>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
         <div className="justify-content-end collapse navbar-collapse" id="navbar">
           <ul className="navbar-nav">
             {routersettings.map((routerItem, index) => (
               <NavLink to={routerItem.redirect} key={index} className="navbar-font hover-color mx-2" activeClassName="active"  exact>
-                <li className="nav-item">
+                <li className="nav-item text-center fs-2">
                   <span className="fw-bold">0{index + 1} </span>
                   {routerItem.name}
                 </li>
